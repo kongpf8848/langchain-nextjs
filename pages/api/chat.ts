64ce09d9de5522d3ce0a0238c95d1663b2c82cc5
Jack kong <kongpf8848@gmail.com>
2023-07-17 18:40:41 +0800

    update

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,9 +1,27 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
+import { OpenAI, OpenAIChat } from "langchain/llms/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage, SystemMessage } from "langchain/schema";
+
 const handler = async(req: NextApiRequest, res: NextApiResponse) => {
-   res.status(200).json({
-    text: "hello world"
-   })
+   const model = new ChatOpenAI({
+      streaming: true,
+      azureOpenAIApiKey: "xxx",
+      azureOpenAIApiInstanceName: "xxx",
+      azureOpenAIApiDeploymentName: "xxx",
+      azureOpenAIApiVersion: "2023-03-15-preview",
+      temperature: 0.9,
+    });
+   const response = await model.call([
+      new SystemMessage(
+         "You are an AI assistant that helps people find information."
+      ),
+      new HumanMessage(
+          "who is elon mask"
+      ),
+  ]);
+  res.status(200).json({ text: response})
 }
 
 export default handler;
