9b5e301a9eaf553f42bc73b2541e486ab34efc1d
kongpf8848 <kongpf8848@gmail.com>
2023-07-23 20:09:17 +0800

    update code

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { LLMChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { CallbackManager } from "langchain/callbacks";
-import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, } from "langchain/prompts";
+import { ChatPromptTemplate, HumanMessagePromptTemplate, PipelinePromptTemplate, PromptTemplate, SystemMessagePromptTemplate, } from "langchain/prompts";
 
 import { OPENAI_API_KEY, OPENAI_API_MODEL,DEFAULT_SYSTEM_PROMPT } from "../../utils/const";
 import { AZURE_OPENAI_API_KEY,AZURE_OPENAI_API_INSTANCE_NAME,AZURE_OPENAI_API_DEPLOYMENT_NAME } from '../../utils/const';
@@ -26,7 +26,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       azureOpenAIApiEmbeddingsDeploymentName:AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME,
       azureOpenAIApiVersion:AZURE_OPENAI_API_VERSION,
       maxTokens: 1000,
-      temperature: 0,
+      temperature: 0.9,
       streaming: true,
       verbose: true,
       callbacks: CallbackManager.fromHandlers({
@@ -55,8 +55,52 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       llm: chat,
    });
 
+   const fullPrompt = PromptTemplate.fromTemplate(`{introduction}
+
+   {example}
+   
+   {start}`);
+   
+   const introductionPrompt = PromptTemplate.fromTemplate(
+     `You are impersonating {person}.`
+   );
+   
+   const examplePrompt =
+     PromptTemplate.fromTemplate(`Here's an example of an interaction:
+   Q: {example_q}
+   A: {example_a}`);
+   
+   const startPrompt = PromptTemplate.fromTemplate(`Now, do this for real!
+   Q: {input}
+   A:`);
+   
+   const composedPrompt = new PipelinePromptTemplate({
+     pipelinePrompts: [
+       {
+         name: "introduction",
+         prompt: introductionPrompt,
+       },
+       {
+         name: "example",
+         prompt: examplePrompt,
+       },
+       {
+         name: "start",
+         prompt: startPrompt,
+       },
+     ],
+     finalPrompt: fullPrompt,
+   });
+   
+   const formattedPrompt = await composedPrompt.format({
+     person: "Elon Musk",
+     example_q: `What's your favorite car?`,
+     example_a: "Telsa",
+     input: `What's your favorite social media site?`,
+   });
+
    await chain.call({
-      text: "Who is Elon Mask?"
+      text: formattedPrompt
    });
 
    res.end()
